3b3350334f16341b66fa386ff2bccb2729343881
Array-colombia <colombiaarray@gmail.com>
2018-04-07 09:23:03 -0500

    abril 7 todo con login google elimine codigo comentado / google auth.php

--- config/social_login/app/clases/google_auth.php
+++ config/social_login/app/clases/google_auth.php
@@ -1,46 +1,4 @@
 <?php 
-	/*//require_once("config/social_login/vendor/autoload.php");
-	class GoogleAuth
-	{
-		protected $client;
-
-		public function __construct(Google_Client $googleClient = null){
-			$this->client = $googleClient;
-			if ($this->client) {
-				$this->client->setClientId('886385635344-j4aic13ehet2s0e7ab3jdlea05ifblag.apps.googleusercontent.com');
-				$this->client->setClientSecret('wCVWwmlhXpu48VjRf3FhOOJf');
-				$this->client->setRedirectUri('http://localhost/%5barray%5d/?:=iniciar-sesion');
-				$this->client->addScope(scope_or_scopes:"https://www.googleapis.com/auth/plus.login");
-
-			}
-		}
-
-		public function GetUrlApiGoogle()
-		{
-			$sair = $this->client->createAuthUrl();
-			return $sair;
-		}
-
-		public function checkRedirectCode()
-		{
-			if (isset($_GET["code"])) 
-			{
-				$this->client->authenticate($_GET["code"]);
-				$this->setToken($this->client->getAccessToken());
-				return true;
-			}else
-			{
-				return false;
-			}
-		}
-
-		public function setToken($token)
-		{
-			$_SESSION["access_token"]=$token;
-			$this->client-setAccessToken($token);
-
-		}
-	}*/
 
 class GoogleAuth
 	{
